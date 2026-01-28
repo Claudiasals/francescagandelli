a43aaa83f990697279cea8ab6d1c80aa284231e5
Claudiasals <salsiniclaudia@gmail.com>
2026-01-28 14:42:36 +0100

    modifica logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import logo from "./assets/images/logo.png";
+import logo from "./assets/images/logo-generico.png";
 import { List, InstagramLogo, Envelope, EnvelopeOpen, PhoneCall } from "phosphor-react";
 
 // Pagine
